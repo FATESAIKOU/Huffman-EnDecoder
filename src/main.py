0db8efe3a6a3138c4ee59aedbd7e4640e5409259
FATESAIKOU <qzsecftbhhhh@gmail.com>
2017-05-07 15:04:22 +0800

    Testing Sequence Defination Complete

--- src/main.py
+++ src/main.py
@@ -5,4 +5,29 @@ This file was wrote for test HEnDecoder
 @author: FATESAIKOU
 """
 
+#from HEnDecoder import HEnDecoder
+
+# get option
+# opt = Utils.GetOption()
+
+# if encode
+#   (heigh, width, image_data) = Utils.LoadImage(filename)
+#
+#   hcer = HEnDecoder()
+#   hcer.SetEncode(image_data, height, width, predict_mode)
+#   hcer.Encode()
+#
+#   Utils.ShowStatus(hcer.GetStatus())
+#
+#   Utils.SaveRaw(filename, hcer.GetEncodeResult())
+#
+# else decode
+#   (heigh, width, t_size, table, content) = Utils.LoadRaw(filename)
+#
+#   hcer = HEnDecoder()
+#   hcer.SetDecode(heigh, width, t_size, table, content)
+#   hcer.Decode()
+#
+#   Utils.SaveImage(filename, heigh, width, hcer.GetDecodeResult())
+
 print "Test Start!"
